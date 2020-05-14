001e8946a25242af4e016f737994d41ecce74f5f
Achyut Timsina <actimsina@gmail.com>
2020-05-14 22:08:45 +0545

    next middleware added

--- routes/taskRouter.js
+++ routes/taskRouter.js
@@ -3,53 +3,53 @@ const Task = require('../models/Task');
 const router = express.Router();
 
 router.route('/')
-    .get((req, res) => {
+    .get((req, res, next) => {
         Task.find()
             .then((tasks) => {
                 res.json(tasks);
-            }).catch((err) => console.log(err));
+            }).catch((err) => next(err));
     })
-    .post((req, res) => {
+    .post((req, res, next) => {
         Task.create(req.body)
             .then((task) => {
                 res.status(201).json(task);
-            }).catch((err) => console.log(err));
+            }).catch((err) => next(err));
     })
-    .delete((req, res) => {
+    .delete((req, res, next) => {
         Task.deleteMany()
             .then((reply) => {
                 res.json(reply);
-            }).catch(err => console.log(err));
+            }).catch(err => next(err));
     });
 
 router.route('/:id')
-    .get((req, res) => {
+    .get((req, res, next) => {
         Task.findById(req.params.id)
             .then((task) => {
                 res.json(task);
-            }).catch(err => console.log(err));
+            }).catch(err => next(err));
     })
-    .put((req, res) => {
+    .put((req, res, next) => {
         Task.findByIdAndUpdate(req.params.id, { $set: req.body }, { new: true })
             .then((task) => {
                 res.json(task);
-            }).catch(err => console.log(err));
+            }).catch(err => next(err));
     })
-    .delete((req, res) => {
+    .delete((req, res, next) => {
         Task.findByIdAndDelete(req.params.id)
             .then((reply) => {
                 res.json(reply);
-            }).catch((err) => console.log(err));
+            }).catch((err) => next(err));
     });
 
 router.route('/:id/notes')
-    .get((req, res) => {
+    .get((req, res, next) => {
         Task.findById(req.params.id)
             .then((task) => {
                 res.json(task.notes);
-            }).catch(err => console.log(err));
+            }).catch(err => next(err));
     })
-    .post((req, res) => {
+    .post((req, res, next) => {
         let newNote = req.body;
         Task.findById(req.params.id)
             .then((task) => {
@@ -57,28 +57,28 @@ router.route('/:id/notes')
                 task.save()
                     .then(newTask => {
                         res.json(newTask.notes)
-                    }).catch(err => console.log(err));
-            }).catch(err => console.log(err));
+                    }).catch(err => next(err));
+            }).catch(err => next(err));
     })
-    .delete((req, res) => {
+    .delete((req, res, next) => {
         Task.findById(req.params.id)
             .then((task) => {
                 task.notes = [],
                     task.save()
                         .then(newTask => {
                             res.json(newTask.notes)
-                        }).catch(err => console.log(err));
-            }).catch(err => console.log(err));
+                        }).catch(err => next(err));
+            }).catch(err => next(err));
     });
 
 router.route('/:id/notes/:noteId')
-    .get((req, res) => {
+    .get((req, res, next) => {
         Task.findById(req.params.id)
             .then((task) => {
                 res.json(task.notes.id(req.params.noteId));
-            }).catch(err => console.log(err));
+            }).catch(err => next(err));
     })
-    .put((req, res) => {
+    .put((req, res, next) => {
         Task.findById(req.params.id)
             .then((task) => {
                 let note = task.notes.id(req.params.noteId);
@@ -86,10 +86,10 @@ router.route('/:id/notes/:noteId')
                 task.save()
                     .then((task) => {
                         res.json(task.notes.id(req.params.noteId));
-                    }).catch(err => console.log(err));
-            }).catch(err => console.log(err));
+                    }).catch(err => next(err));
+            }).catch(err => next(err));
     })
-    .delete((req, res) => {
+    .delete((req, res, next) => {
         Task.findById(req.params.id)
             .then((task) => {
                 task.notes = task.notes.filter((note) => {
@@ -98,8 +98,8 @@ router.route('/:id/notes/:noteId')
                 task.save()
                     .then((task) => {
                         res.json(task.notes);
-                    }).catch(err => console.log(err));
-            }).catch(err => console.log(err));
+                    }).catch(err => next(err));
+            }).catch(err => next(err));
     });
 
 module.exports = router;
