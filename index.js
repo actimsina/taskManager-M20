b2ae6f123db8c1cd5e3676072fae0ac0abd0e603
Achyut Timsina <actimsina@gmail.com>
2020-05-16 08:18:04 +0545

    dotenv added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 const categoryRouter = require('./routes/categoryRouter');
 const taskRouter = require('./routes/taskRouter');
@@ -7,7 +8,7 @@ const taskRouter = require('./routes/taskRouter');
 
 const app = express();
 
-mongoose.connect('mongodb://127.0.0.1:27017/TaskManagerMarch2020', {
+mongoose.connect(process.env.DbURI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(() => {
@@ -24,7 +25,6 @@ app.get('/', (req, res) => {
 app.use('/api/categories', categoryRouter);
 app.use('/api/tasks', taskRouter);
 
-
-app.listen(3000, () => {
-    console.log('Server is running at localhost:3000');
+app.listen(process.env.Port, () => {
+    console.log(`Server is running at localhost:${process.env.Port}`);
 });
