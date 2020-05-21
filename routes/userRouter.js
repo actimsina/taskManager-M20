e7e0f410cb3daaeacfc78878ad33309199c59a1e
Achyut Timsina <actimsina@gmail.com>
2020-05-21 10:52:50 +0545

    users register login added

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -5,30 +5,58 @@ const User = require('../models/User');
 
 const router = express.Router();
 
-router.post('/signup', (req, res, next) => {
+router.post('/register', (req, res, next) => {
     let { username, password, firstName, lastName, role } = req.body;
     User.findOne({ username })
         .then(user => {
             if (user) {
                 let err = new Error('Username already exists!');
-                err.status = 401;
+                err.status = 400;
                 return next(err);
             }
             bcrypt.hash(password, 10)
                 .then((hash) => {
+                    User.create({ username, password: hash, firstName, lastName, role })
+                        .then(user => {
+                            res.status(201).json({ "status": "Signup Success" });
+                        }).catch(next);
+                }).catch(next);
+        }).catch(next);
+});
+
+router.post('/login', (req, res, next) => {
+    let { username, password } = req.body;
+    User.findOne({ username })
+        .then((user) => {
+            if (!user) {
+                let err = new Error('User does not exists!');
+                err.status = 400;
+                return next(err);
+            }
+            bcrypt.compare(password, user.password)
+                .then(isMatch => {
+                    if (!isMatch) {
+                        let err = new Error('Password does not match!');
+                        err.status = 401;
+                        return next(err);
+                    }
                     let payload = {
+                        id: user.id,
                         username: user.username,
                         firstName: user.firstName,
                         lastName: user.lastName,
                         role: user.role
                     }
-                    let token = jwt.sign(payload, process.env.SECRET);
-                    User.create({ username, password: hash, firstName, lastName })
-                        .then(user => {
-                            res.status(201).json(token);
-                        }).catch(next);
+                    jwt.sign(payload, process.env.SECRET, (err, token) => {
+                        if (err) { throw new Error('Could not issue a token'); }
+                        res.json({
+                            success: true,
+                            token: `Bearer ${token}`
+                        });
+                    });
                 }).catch(next);
         }).catch(next);
 });
+
 
 module.exports = router;
